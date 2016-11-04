e1655f5f6421de615be4cdcab9007e24bc2a2042
Nikolas <nxexenis@gmail.com>
2016-11-04 20:19:08 +1100

    Added new test and documentation

--- src/test/java/BlockTest.java
+++ src/test/java/BlockTest.java
@@ -10,6 +10,44 @@ import java.util.Set;
  */
 public class BlockTest
 {
+
+    /**
+     * Ensure the child set returned is a copy of the original (i.e. not a reference)
+     */
+    @Test
+    public void testGetChildrenCopy()
+    {
+        // Initialise blocks
+        Block b1 = new Block("B1");
+        Block b2 = new Block("B2");
+        Block b3 = new Block("B3");
+        Block b4 = new Block("B4");
+        Block b5 = new Block("B5");
+
+        // Add children to B1
+        b1.addChild(b2);
+        b1.addChild(b3);
+        b1.addChild(b4);
+        b1.addChild(b5);
+
+        // Check that the returned set is a copy
+        Set<Block> original = b1.getChildren();
+        Set<Block> changed = b1.getChildren();
+        changed.clear();
+        Set<Block> current = b1.getChildren();
+
+        // The child set within B1 should not have changed
+        Assert.assertEquals(original, current);
+        Assert.assertNotEquals(current, changed);
+        Assert.assertNotEquals(original, changed);
+    }
+
+    /**
+     * Ensure that the block tree printing works as expected
+     * - Spacing is correct
+     * - Correct children are printed
+     * - Appropriately stop recursion when a cycle is encountered
+     */
     @Test
     public void testBlockTreeString()
     {
